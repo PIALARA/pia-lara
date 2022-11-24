4d931565c932b0fd47b962ca7e8b81ec92a78de1
Jesus <jesus@jesusyepes.com>
2022-11-24 19:45:21 +0100

    Update syllabus.py
    Ejemplo de estructura de un controlador en un BluePrint

--- pialara/blueprints/syllabus.py
+++ pialara/blueprints/syllabus.py
@@ -8,3 +8,30 @@ from pialara.db import get_db
 @bp.route('/')
 def index():
     return render_template('syllabus/index.html')
+
+@bp.route('/create/:id')
+def create():
+    return render_template('syllabus/create.html')
+
+@bp.route('/create/:id', methods=['post'])
+def create_post():
+    flash('creado correctamente')
+    redirect('/create')
+
+@bp.route('/update/:id')
+def update():
+    return render_template('syllabus/update.html')
+
+@bp.route('/update/:id', methods=['post'])
+def update_post():
+    flash('modificado correctamente')
+    redirect('/update')
+
+
+@bp.route('/delete/:id')
+def delete():
+    return render_template('syllabus/index.html')
+
+@bp.route('/view/:id')
+def view():
+    return render_template('syllabus/index.html')
