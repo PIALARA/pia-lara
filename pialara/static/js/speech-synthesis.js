11c1945d04e044b63f008eca378377914be2175a
adrian-iabd <118607624+adrian-iabd@users.noreply.github.com>
2022-12-13 21:09:25 +0100

    Añadir template de grabación con texto propio

--- pialara/static/js/speech-synthesis.js
+++ pialara/static/js/speech-synthesis.js
@@ -2,7 +2,6 @@ const syllabusText = document.querySelector('#syllabus-text');
 const playButton = document.querySelector('#play-button');
 
 const audio = new SpeechSynthesisUtterance();
-audio.text = syllabusText.textContent;
 
 audio.addEventListener('end', () => {
   playButton.classList.replace('btn-danger', 'btn-warning');
@@ -10,6 +9,8 @@ audio.addEventListener('end', () => {
 });
 
 playButton.addEventListener('click', () => {
+  audio.text = syllabusText.value || syllabusText.textContent;
+
   if (window.speechSynthesis.speaking) {
     playButton.classList.replace('btn-danger', 'btn-warning');
     playButton.innerHTML = '<i class="bi bi-ear me-2"></i>Escuchar frase';
