ac13b8d79eeb47963fcc08d27e6510de04002fa0
Jesus Yepes <jesus@jesusyepes.com>
2022-12-13 18:27:37 +0100

    Template para el rol cliente. Tres rutas para el flujo que se definió ayer que iba a tener la aplicación de cliente (ver blueprints/audios)

--- pialara/blueprints/audios.py
+++ pialara/blueprints/audios.py
@@ -2,6 +2,19 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('audios', __name__, url_prefix='/audios')
 
+@bp.route('/client-tag')
+def client_tag():
+    return render_template('audios/client_tag.html')
+
+@bp.route('/client-record')
+def client_record():
+    return render_template('audios/client_record.html')
+
+@bp.route('/client-text')
+def client_text():
+    return render_template('audios/client_text.html')
+
+## @todo deleteme
 @bp.route('/create')
 def index():
     return render_template('audios/create.html')
