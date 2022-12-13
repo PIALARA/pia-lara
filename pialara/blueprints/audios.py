d78102b96c4ff113d59e9632ea9c452b782c83fb
carlosmoncho <91116344+carlosmoncho@users.noreply.github.com>
2022-12-13 18:44:43 +0100

    49 grabación de audios (#74)
    * Añadir grabación de audios
    
    * save audio
    
    * Utilizar el mismo botón para grabar y parar
    
    * Pasar audio a audios.py usando ajax
    
    * modificacion html create
    
    Co-authored-by: adrian-iabd <118607624+adrian-iabd@users.noreply.github.com>
    Co-authored-by: carlos <carlosmoncho2002@gmail.com>

--- pialara/blueprints/audios.py
+++ pialara/blueprints/audios.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template
+from flask import (
+    Blueprint, render_template, request
+)
+
 
 bp = Blueprint('audios', __name__, url_prefix='/audios')
 
@@ -18,3 +22,9 @@ def client_text():
 @bp.route('/create')
 def index():
     return render_template('audios/create.html')
+
+@bp.route('/save-record', methods=['POST'])
+def save_record():
+    file = request.files['file']
+    return render_template('audios/create.html')
+
---
+++ pialara/static/js/speech-recording.js
@@ -0,0 +1,59 @@
+const recordButton = document.querySelector('#record-button');
+const recordedAudio = document.querySelector('#recorded-audio');
+const sendAudio = document.querySelector('#send-button');
+
+let audioChunks = [];
+let generalBlob = '';
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+  handlerFunction(stream);
+});
+
+function handlerFunction(stream) {
+  mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.addEventListener('dataavailable', e => {
+    audioChunks.push(e.data);
+    if (mediaRecorder.state === 'inactive') {
+      let blob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+      console.log(blob);
+      // sendData(blob);
+      const url = URL.createObjectURL(blob);
+      recordedAudio.controls = true;
+      recordedAudio.src = url;
+      generalBlob = blob
+    }
+  });
+}
+
+recordButton.addEventListener('click', e => {
+  if (mediaRecorder.state === 'inactive') {
+    console.log('Recording are started..');
+    recordButton.classList.replace('btn-success', 'btn-danger');
+    recordButton.innerHTML = '<i class="bi bi-stop me-2"></i>Parar de grabar';
+    recordedAudio.controls = false;
+    audioChunks = [];
+    mediaRecorder.start();
+  } else if (mediaRecorder.state === 'recording') {
+    console.log('Recording are stopped.');
+    recordButton.classList.replace('btn-danger', 'btn-success');
+    recordButton.innerHTML = '<i class="bi bi-mic me-2"></i>Volver a grabar';
+    mediaRecorder.stop();
+  }
+});
+
+sendAudio.addEventListener('click', e => {
+  var form = new FormData();
+  form.append('file', generalBlob, URL.createObjectURL(generalBlob));
+  form.append('title', 'data.mp3');
+  //Chrome inspector shows that the post data includes a file and a title.
+  $.ajax({
+      type: 'POST',
+      url: '/audios/save-record',
+      data: form,
+      cache: false,
+      processData: false,
+      contentType: false
+  }).done(function(data) {
+      window.location.href = "create";
+  });
+});
