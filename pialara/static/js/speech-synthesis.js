404edbe851f32883238aa3d6b1c02c4c4a481533
adrian-iabd <118607624+adrian-iabd@users.noreply.github.com>
2022-12-14 20:51:24 +0100

    Estilo de los botones

--- pialara/static/js/speech-synthesis.js
+++ pialara/static/js/speech-synthesis.js
@@ -5,7 +5,10 @@ const audio = new SpeechSynthesisUtterance();
 
 audio.addEventListener('end', () => {
   playButton.classList.replace('btn-danger', 'btn-warning');
-  playButton.innerHTML = '<i class="bi bi-ear me-2"></i>Escuchar frase';
+  playButton.innerHTML = `<span>
+      <span><i class="bi bi-ear me-2"></i></span>
+      <span>Escuchar frase</span>
+    </span>`;
 });
 
 playButton.addEventListener('click', () => {
@@ -13,11 +16,17 @@ playButton.addEventListener('click', () => {
 
   if (window.speechSynthesis.speaking) {
     playButton.classList.replace('btn-danger', 'btn-warning');
-    playButton.innerHTML = '<i class="bi bi-ear me-2"></i>Escuchar frase';
+    playButton.innerHTML = `<span>
+      <span><i class="bi bi-ear me-2"></i></span>
+      <span>Escuchar frase</span>
+    </span>`;
     window.speechSynthesis.cancel();
   } else {
     playButton.classList.replace('btn-warning', 'btn-danger');
-    playButton.innerHTML = '<i class="bi bi-stop me-2"></i>Dejar de escuchar';
+    playButton.innerHTML = `<span>
+      <span><i class="bi bi-stop me-2"></i></span>
+      <span>Dejar de escuchar</span>
+    </span>`;
     window.speechSynthesis.speak(audio);
   }
 });
