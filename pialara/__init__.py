3494558b55386fa346911b1af75a3c8f2660a8b5
Jesus Yepes <jesus@jesusyepes.com>
2023-02-13 19:16:25 +0100

    CF

--- pialara/__init__.py
+++ pialara/__init__.py
@@ -14,12 +14,12 @@ def create_app():
     # config.read(os.path.abspath(os.path.join(".ini")))
     config.read('/var/www/pia-lara/pialara/.ini')
 
-    app.config['PIALARA_DB_URI'] = config['LOCAL']['PIALARA_DB_URI']
-    app.config['PIALARA_DB_NAME'] = config['LOCAL']['PIALARA_DB_NAME']
-    app.config['SECRET_KEY'] = config['LOCAL']['SECRET_KEY']
-
-    app.config['AWS_ACCESS_KEY_ID'] = config['LOCAL']['AWS_ACCESS_KEY_ID']
-    app.config['AWS_SECRET_ACCESS_KEY'] = config['LOCAL']['AWS_SECRET_ACCESS_KEY']
+    # app.config['PIALARA_DB_URI'] = config['LOCAL']['PIALARA_DB_URI']
+    # app.config['PIALARA_DB_NAME'] = config['LOCAL']['PIALARA_DB_NAME']
+    # app.config['SECRET_KEY'] = config['LOCAL']['SECRET_KEY']
+    #
+    # app.config['AWS_ACCESS_KEY_ID'] = config['LOCAL']['AWS_ACCESS_KEY_ID']
+    # app.config['AWS_SECRET_ACCESS_KEY'] = config['LOCAL']['AWS_SECRET_ACCESS_KEY']
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
