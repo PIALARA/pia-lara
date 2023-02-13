01363f994b3c83fb3427235b8508b8a1d871cd0d
Jesus Yepes <jesus@jesusyepes.com>
2023-02-13 19:08:27 +0100

    CF

--- pialara/__init__.py
+++ pialara/__init__.py
@@ -4,14 +4,14 @@ from pialara import db
 import os
 import configparser
 
-config = configparser.ConfigParser()
-config.read(os.path.abspath(os.path.join(".ini")))
+
 
 
 def create_app():
     # create and configure the app
     app = Flask(__name__)
-
+    config = configparser.ConfigParser()
+    config.read(os.path.abspath(os.path.join(".ini")))
     app.config['PIALARA_DB_URI'] = config['LOCAL']['PIALARA_DB_URI']
     app.config['PIALARA_DB_NAME'] = config['LOCAL']['PIALARA_DB_NAME']
     app.config['SECRET_KEY'] = config['LOCAL']['SECRET_KEY']
