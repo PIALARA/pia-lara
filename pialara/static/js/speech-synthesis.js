e3e714b78d6a6d204145261f0cb0d999be8778a6
carpe <carpeas@outlook.es>
2023-11-17 13:19:01 +0100

    usabilidad_velocidad_audios

--- pialara/static/js/speech-synthesis.js
+++ pialara/static/js/speech-synthesis.js
@@ -1,7 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const syllabusText = document.querySelector('#syllabus-text');
 const playButton = document.querySelector('#play-button');
 const showSyllabusButton = document.querySelector('.show-syllabus-button');
 const showSyllabusButtonText = document.querySelector('.show-syllabus-button-text');
+
+//botones audio
+const speedButtonsContainer = document.querySelector('.speed-buttons');
+const speedButtons = document.querySelectorAll('.speed-button');
+//botones audio fin
 
 const audio = new SpeechSynthesisUtterance();
 
@@ -12,25 +19,45 @@ audio.addEventListener('end', () => {
     <span>Escuchar</span>`;
 });
 
-playButton.addEventListener('click', () => {
-  audio.text = syllabusText.value || syllabusText.textContent;
-
-  if (window.speechSynthesis.speaking) {
-    playButton.classList.replace('btn-danger', 'btn-warning');
-    playButton.innerHTML = `
-      <span><i class="bi bi-ear me-2"></i></span>
-      <span>Escuchar</span>`;
-    window.speechSynthesis.cancel();
-  } else {
-    playButton.classList.replace('btn-warning', 'btn-danger');
-    playButton.innerHTML = `
-      <span><i class="bi bi-stop me-2"></i></span>
-      <span>Parar</span>`;
-    window.speechSynthesis.speak(audio);
-  }
-});
-
+console.log('Entro sistesis1:',playButton);
+//botones audio  //Se inserta el if sino da error la primera vez que entra aqui (warning)
+if(playButton != null)
+{
+  playButton.addEventListener('click', () => {
+    audio.text = syllabusText.value || syllabusText.textContent;
+    console.log('Entro sistesis:2');
+    if (window.speechSynthesis.speaking) {
+      playButton.classList.replace('btn-danger', 'btn-warning');
+      playButton.innerHTML = `
+        <span><i class="bi bi-ear me-2"></i></span>
+        <span>Escuchar</span>`;
+      console.log('Entro paro mitad:');
+      window.speechSynthesis.cancel();
+    } else {
+      playButton.classList.replace('btn-warning', 'btn-danger');
+      playButton.innerHTML = `
+        <span><i class="bi bi-stop me-2"></i></span>
+        <span>Parar</span>`;
+      window.speechSynthesis.speak(audio);
+       
+      console.log('Entro escucho final:');
+      
+    }
+    //botones audio //ajustes botones cuando le doy a escuchar
+    speedButtonsContainer.style.display = 'flex';
+    speedButtons.forEach(button => {
+      button.addEventListener('click', () => {
+          const speedFactor = parseFloat(button.dataset.speed); // Obtener la velocidad desde el atributo 'data-speed'
+          audio.rate = speedFactor;
+        });
+    })
+    //fin botones audio
+  });
+}
+console.log('Entro sistesis3:');
 showSyllabusButton && showSyllabusButton.addEventListener('click', () => {
+  console.log('Entro sistesis4:');
   syllabusText.classList.toggle('hide-syllabus-text');
   showSyllabusButtonText.textContent = syllabusText.classList.contains('hide-syllabus-text') ? 'Mostrar' : 'Ocultar';
 });
+})
