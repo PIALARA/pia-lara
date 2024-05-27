d0c37a53fbc193642f16dab13db089fd1b18a9b8
Aitor Medrano <aitormedrano@gmail.com>
2024-05-27 20:46:20 +0200

    Quitado Gradio de momento

--- pialara/blueprints/lara.py
+++ pialara/blueprints/lara.py
@@ -2,7 +2,7 @@ import os, uuid
 from flask import (
     Blueprint, render_template, request, current_app
 )
-from gradio_client import Client, file
+# from gradio_client import Client, file
 
 bp = Blueprint('lara', __name__)
 rutaAudio = "audio_files/"
@@ -11,23 +11,24 @@ rutaAudio = "audio_files/"
 def login():
     return render_template('lara/lara.html')
 
-@bp.route('/lara/save_record', methods=['POST'])
-def save_record():
-    print("Guardando audio")
-    audio_file = request.files['audio_data']
-    if not os.path.isdir('audio_files'):
-        os.makedirs('audio_files')
 
-    name_audio = str(uuid.uuid4()) + '.wav'
-    audio_file.save(os.path.join('audio_files', name_audio))
+# @bp.route('/lara/save_record', methods=['POST'])
+# def save_record():
+#     print("Guardando audio")
+#     audio_file = request.files['audio_data']
+#     if not os.path.isdir('audio_files'):
+#         os.makedirs('audio_files')
 
-    GRADIO_URL = current_app.config['GRADIO_URL']
+#     name_audio = str(uuid.uuid4()) + '.wav'
+#     audio_file.save(os.path.join('audio_files', name_audio))
 
-    client = Client(GRADIO_URL, ssl_verify=False)
-    transcription = client.predict(
-            audio=file(rutaAudio + name_audio),
-            modelo="base",
-            api_name="/predict"
-    )
+#     GRADIO_URL = current_app.config['GRADIO_URL']
 
-    return {'status': 'ok', 'text': transcription}
+#     client = Client(GRADIO_URL, ssl_verify=False)
+#     transcription = client.predict(
+#             audio=file(rutaAudio + name_audio),
+#             modelo="base",
+#             api_name="/predict"
+#     )
+
+#     return {'status': 'ok', 'text': transcription}
